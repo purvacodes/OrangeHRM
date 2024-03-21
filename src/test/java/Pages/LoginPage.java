15164e843b61f36c596b7b88c1fefe0a4bdf18e5
purvacodes <purvamalhotradt@gmail.com>
2024-03-21 13:29:44 +0530

    Setting up the project

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,2 +1,17 @@
-package Pages;public class LoginPage {
+package Pages;
+
+import Utility.Base;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class LoginPage extends Base {
+
+    @Test
+    public void LoginToOrangeHrm() throws InterruptedException {
+        navigateToOrangeHRM();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button")).click();
+    }
 }
--- src/test/java/Utility/Base.java
+++ src/test/java/Utility/Base.java
@@ -1,2 +1,25 @@
-package Utility;public class Base {
+package Utility;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+
+public class Base {
+    public static WebDriver driver;
+    @BeforeTest
+    public void initializeBrowser() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+    }
+    public void navigateToOrangeHRM() {
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().deleteAllCookies();
+        driver.navigate().refresh();
+    }
+
 }
